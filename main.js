6cc6b47fe31c2a88606846dbf38af559374bbd79
mmxwillow <mxwierzba@gmail.com>
2022-10-21 11:19:34 +0200

    File init

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("hi");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGtCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbmV3LXJlc3RhdXJhbnQvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJoaVwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
