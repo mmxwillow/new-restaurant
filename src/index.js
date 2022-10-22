2578c49465b20536aa46ef297d55f272aa8c776e
mmxwillow <mxwierzba@gmail.com>
2022-10-22 16:09:45 +0200

    Homepage UI design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hi");
+const displayMenu = (() => {
+    document.querySelector('#menu').addEventListener('click', () => {
+        document.querySelector('ul').classList.toggle('visible');
+    })
+})();
