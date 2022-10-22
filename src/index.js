ebd80cd152d08c2f31fe12df6c53b319339bdf42
mmxwillow <mxwierzba@gmail.com>
2022-10-22 17:25:45 +0200

    Printing header from JS

--- src/index.js
+++ src/index.js
@@ -12,8 +12,32 @@
 */
 import printHome from './home.js'
 
+const displayHeader = (() => {
+    const header = document.createElement('header');
+    const list = document.createElement('ul');
+    const menu = document.createElement('span');
+
+    const links =['Home', 'Menu', 'Contact'];
+
+    links.forEach((link) => {
+        const li = document.createElement('li');
+        li.innerHTML = link;
+        list.appendChild(li);
+    })
+
+    menu.id = 'menu';
+    menu.classList.add("material-symbols-outlined");
+    menu.innerHTML = "menu";
+
+    menu.addEventListener('click', () => {
+        document.querySelector('ul').classList.toggle('visible');
+    })
+
+    header.appendChild(list);
+    header.appendChild(menu);
+
+    document.querySelector('#content').appendChild(header);
+})();
+
 printHome();
 
-document.querySelector('#menu').addEventListener('click', () => {
-    document.querySelector('ul').classList.toggle('visible');
-})
